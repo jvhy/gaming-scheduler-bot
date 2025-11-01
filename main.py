d6c610e47375758fe1bf9a6b61bb6597d22d00ad
Jussi-Veikka Hynynen <jussiveikka.hynynen@gmail.com>
2025-11-01 14:01:17 +0200

    Add basic bot setup

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from gaming-scheduler-bot!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/main.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+import os
+import logging
+
+
+load_dotenv()
+token = os.getenv("DISCORD_TOKEN")
+
+handler = logging.FileHandler(filename="bot.log", encoding="utf-8", mode="w")
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} is ready!")
+
+
+def main():
+    bot.run(token, log_handler=handler, log_level=logging.DEBUG)
+
+
+if __name__ == "__main__":
+    main()
